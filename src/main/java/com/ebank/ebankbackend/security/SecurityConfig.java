7374dd8435b2a2a98e1693d6bf023e48f75cf8c7
Abdellah <abdallahkaba98@gmail.com>
2024-03-22 00:54:19 +0000

    jwt-2bean-jwt-encoder-jwt-decoder

--- src/main/java/com/ebank/ebankbackend/security/SecurityConfig.java
+++ src/main/java/com/ebank/ebankbackend/security/SecurityConfig.java
@@ -1,21 +1,35 @@
 package com.ebank.ebankbackend.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //L'anotation @Value() permet de recuperer une variable d'environnement
+    @Value("${jwt.secret}")
+    private String secretKey ;
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager () {
@@ -39,7 +53,20 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(oa->oa.jwt(Customizer.withDefaults()))
                 .build() ;
     }
+
+    @Bean
+    JwtEncoder jwtEncoder () {
+        //String secretKey = "d98jjkdfsda09dfdskfsdjfsaldjfkdsdj8h7s7hbvxc3209azdhd12j485jkff7";
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+    }
+    @Bean
+    JwtDecoder jwtDecoder () {
+        //String secretKey = "d98jjkdfsda09dfdskfsdjfsaldjfkdsdj8h7s7hbvxc3209azdhd12j485jkff7";
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(),"RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
+    }
+
 }
--- src/main/java/com/ebank/ebankbackend/security/SecurityController.java
+++ src/main/java/com/ebank/ebankbackend/security/SecurityController.java
@@ -1,6 +1,7 @@
 package com.ebank.ebankbackend.security;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class SecurityController {
+
+
 
     //un controller pour consulter le profil de l'utilisateur
     @GetMapping("/profile")
